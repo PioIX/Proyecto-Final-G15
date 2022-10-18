a4edd93d99e449d5c821678e9fe7a01261b3e96b
meliescalante <101184949+meliescalante@users.noreply.github.com>
2022-10-18 11:30:05 -0300

    Update javascript.js

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -3,7 +3,7 @@ function signUp() {
   contraLogin = document.getElementById("password").value
  
   $.ajax({ 
-    url:"/ingresar", 
+    url:"/verificar", 
     type:"POST", 
     data: {"value": nombreLogin,
           "value2": contraLogin}, 
@@ -11,12 +11,65 @@ function signUp() {
     success: function(response){  //En response voy a tener el JSON
       datos = (response); //
 
-      alert(`Ha sido resgistrado correctamente`)
-      location.href = "/administrador"
+      
       //document.getElementById("form1").submit()
+      if (datos){
+        alert(`Ha sido ingresado correctamente`)
+        location.href = "/administrador"
+      }
+      else{
+        alert("Datos incorrectos")
+        location.href = "/inicio"
+      }
+      
+      
+    }, 
+    error: function(error){ 
+      console.log(error); 
+  }, });
+
+}
+function index() {
+  informacion = document.getElementById("informacion").value
+  fotos = document.getElementById("fotos").value
+
+  $.ajax({ 
+    url:"/agregarIndex", 
+    type:"POST", 
+    data: {"value": informacion,
+          "value2": fotos,
+          }, 
+
+    success: function(response){  //En response voy a tener el JSON
+      datos = (response); 
+
+     alert(`Ha sido agregado correctamente`)
+    
     }, 
     error: function(error){ 
       console.log("hola"); 
+  }, });
+}
+
+function borrarIndex(){
+  informacionBorrar = document.getElementById("informacion").value
+  fotosBorrar = document.getElementById("fotos").value
+
+  $.ajax({
+    url:"/borrarIndex",
+    type:"DELETE",
+    data: {"value": informacion,
+           "value2": fotos,
+          },
+
+    success: function(response){
+      datos = response
+      
+      alert("Se ha eliminado correctamente")
+      
+    },
+    error: function(error){
+      console.log(error);
   }, });
 
 }
@@ -33,7 +86,7 @@ function museos() {
   logo= document.getElementById("logo").value
  
   $.ajax({ 
-    url:"/guardarMuseo", 
+    url:"/agregarMuseo", 
     type:"POST", 
     data: {"value": titulo,
           "value2": informacion,
@@ -47,61 +100,48 @@ function museos() {
           }, 
 
     success: function(response){  //En response voy a tener el JSON
-      datos = (response); //
+      datos = (response); 
 
       alert(`Ha sido agregado correctamente`)
     
     }, 
     error: function(error){ 
-      console.log("hola"); 
+      console.log(error); 
   }, });
 
 }
 function internacional() {
-  pregunta1 = document.getElementById("campo1").value 
-  respuesta1 = document.getElementById("textoCampo1").value
-  pregunta2 = document.getElementById("foto1").value 
-  respuesta2 = document.getElementById("campo2").value
-  pregunta3 = document.getElementById("textoCampo2").value 
-  respuesta3 = document.getElementById("foto2").value
-  pregunta4 = document.getElementById("campo3").value 
-  respuesta4 = document.getElementById("textoCampo3").value
-  pregunta5 = document.getElementById("foto3").value 
+  campo1 = document.getElementById("campo1").value 
+  textoCampo1 = document.getElementById("textocampo1").value
+  fotoCampo1 = document.getElementById("fotoCampo1").value 
+  campo2 = document.getElementById("campo2").value
+  textoCampo2 = document.getElementById("textocampo2").value 
+  fotoCampo2 = document.getElementById("fotoCampo2").value
+  campo3 = document.getElementById("campo3").value 
+  textoCampo3 = document.getElementById("textocampo3").value
+  fotoCampo3 = document.getElementById("fotoCampo3").value 
   
-  console.log(pregunta1)
-  console.log(respuesta1)
-  console.log(pregunta2)
-  console.log(respuesta2)
-  console.log(pregunta3)
-  console.log(respuesta3)
-  console.log(pregunta4)
-  console.log(respuesta4)
-  console.log(pregunta5)
-  console.log(respuesta5)
   $.ajax({ 
-    url:"/guardarPreguntas", 
+    url:"/agregarInternacional", 
     type:"POST", 
-    data: {"value": pregunta1,
-          "value2": respuesta2,
-          "value3": pregunta2,
-          "value4": respuesta2,
-          "value5": pregunta3,
-          "value6": respuesta3,
-          "value7": pregunta4,
-          "value8": respuesta4,
-          "value9":pregunta5,
-          "value10": respuesta5,
+    data: {"value": campo1,
+          "value2": textoCampo1,
+          "value3": fotoCampo1,
+          "value4": campo2,
+          "value5": textoCampo2,
+          "value6": fotoCampo2,
+          "value7": campo3,
+          "value8": textoCampo3,
+          "value9": fotoCampo3,
           }, 
 
     success: function(response){  //En response voy a tener el JSON
-      datos = (response); //
+      datos = (response); 
 
       alert(`Ha sido agregado correctamente`)
       
-      //document.getElementById("form1").submit()
     }, 
     error: function(error){ 
-      console.log("hola"); 
       console.log(error); 
   }, });
 
@@ -117,18 +157,9 @@ function preguntas() {
   respuesta4 = document.getElementById("respuesta4").value
   pregunta5 = document.getElementById("pregunta5").value 
   respuesta5 = document.getElementById("respuesta5").value
-  console.log(pregunta1)
-  console.log(respuesta1)
-  console.log(pregunta2)
-  console.log(respuesta2)
-  console.log(pregunta3)
-  console.log(respuesta3)
-  console.log(pregunta4)
-  console.log(respuesta4)
-  console.log(pregunta5)
-  console.log(respuesta5)
+ 
   $.ajax({ 
-    url:"/guardarPreguntas", 
+    url:"/agregarPreguntas", 
     type:"POST", 
     data: {"value": pregunta1,
           "value2": respuesta2,
@@ -138,16 +169,15 @@ function preguntas() {
           "value6": respuesta3,
           "value7": pregunta4,
           "value8": respuesta4,
-          "value9":pregunta5,
+          "value9": pregunta5,
           "value10": respuesta5,
           }, 
 
     success: function(response){  //En response voy a tener el JSON
-      datos = (response); //
+      datos = (response); 
 
       alert(`Ha sido agregado correctamente`)
-      
-      //document.getElementById("form1").submit()
+  
     }, 
     error: function(error){ 
       console.log("hola"); 
@@ -155,9 +185,65 @@ function preguntas() {
   }, });
 
 }
+function borrarPreguntas(){
+  SelectEliminar = document.getElementById('SelectEliminar').value
+
+  $.ajax({
+    url:"/borrarPreguntas",
+    type:"DELETE",
+    data: {"value": SelectEliminar,
+          
+          
+          }, 
+
+    success: function(response){
+      datos = response
+      alert("La definición se ha eliminado correctamente")
+      
+    },
+    error: function(error){
+      console.log(error);
+  }, });
+}
+
+function novedades(){
+  foto1=document.getElementById("foto1").value
+  texto1=document.getElementById("texto1").value
+  foto2=document.getElementById("foto2").value
+  texto2=document.getElementById("texto2").value
+  foto3=document.getElementById("foto3").value
+  texto3=document.getElementById("texto3").value
+  foto4=document.getElementById("foto4").value
+  texto4=document.getElementById("texto4").value
+  link=document.getElementById("link").value
+ 
+  $.ajax({ 
+    url:"/agregarNovedades", 
+    type:"POST", 
+    data: {"value": foto1,
+          "value2": texto1,
+          "value3": foto2,
+          "value4": texto2,
+          "value5": foto3,
+          "value6": texto3,
+          "value7": foto4,
+          "value8": link,
+          }, 
+
+    success: function(response){  //En response voy a tener el JSON
+      datos = (response); 
+
+      alert(`Ha sido agregado correctamente`)
+
+    }, 
+    error: function(error){ 
+      console.log(error); 
+  }, });
+
+}
 function museosi(){
-  document.getElementById("ModificarTuPagina").hidden = true
-  document.getElementById("ModificarMuseos").hidden = false
+  document.getElementById("modificarTuPagina").hidden = true
+  document.getElementById("modificarMuseos").hidden = false
   document.getElementById("modificarNovedades").hidden = true
   document.getElementById("modificarInternacional").hidden = true
   document.getElementById("modificarPreguntas").hidden = true
@@ -165,47 +251,28 @@ function museosi(){
 }
 
 function novedadesi(){
-  document.getElementById("ModificarTuPagina").hidden = true
-  document.getElementById("ModificarMuseos").hidden = true
+  document.getElementById("modificarTuPagina").hidden = true
+  document.getElementById("modificarMuseos").hidden = true
   document.getElementById("modificarNovedades").hidden = false
   document.getElementById("modificarInternacional").hidden = true
   document.getElementById("modificarPreguntas").hidden = true
     
 }
 function internacionali(){
-  document.getElementById("ModificarTuPagina").hidden = true
-  document.getElementById("ModificarMuseos").hidden = true
+  document.getElementById("modificarTuPagina").hidden = true
+  document.getElementById("modificarMuseos").hidden = true
   document.getElementById("modificarNovedades").hidden = true
   document.getElementById("modificarInternacional").hidden = false
   document.getElementById("modificarPreguntas").hidden = true
   
 }
 function preguntasi(){
-  document.getElementById("ModificarTuPagina").hidden = true
-  document.getElementById("ModificarMuseos").hidden = true
+  document.getElementById("modificarTuPagina").hidden = true
+  document.getElementById("modificarMuseos").hidden = true
   document.getElementById("modificarNovedades").hidden = true
   document.getElementById("modificarInternacional").hidden = true
   document.getElementById("modificarPreguntas").hidden = false
   
   
-}
-function rellenarInfo{
-  $.ajax({ 
-    url:"/faq", 
-    type:"GET", 
-    data: {"value": pregunta1,
-          "value2": respuesta1}, 
-
-    success: function(response){  //En response voy a tener el JSON
-      datos = (response); //
-
-      //alert(`Ha sido resgistrado correctamente`)
-      
-      //document.getElementById("form1").submit()
-    }, 
-    error: function(error){ 
-      console.log(error); 
-  }, });
-  document.getElementById("summary1").innerHTML=rellenarInfo()
 }
  
