a3b4a7724fbe341922ead71b8981b0d6d21d6dc0
meliescalante <101184949+meliescalante@users.noreply.github.com>
2022-10-28 21:39:43 -0300

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, request, jsonify
-import sqlite3
+from flask import Flask, render_template, request, jsonify, redirect
+import sqlite3, os
 
+from os.path import abspath, dirname, join
+from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = './media'
+UPLOAD_FOLDER = './static/media'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-path = './media'
+path = './static'
 path2 = 'img'
 
 app = Flask(__name__)
-
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.secret_key = 'clave'
-
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  conn = sqlite3.connect("artemis.db")
+  resu=conn.execute(f"""SELECT * FROM CualquierCosa """)
+  respuestaDelIndex= resu.fetchall()
+  conn.close()
+  largoImagen=len(respuestaDelIndex)
+  return render_template('index.html',respuestaDelIndex = respuestaDelIndex, largoImagen=len(respuestaDelIndex))
+  
 
 @app.route('/museos2', methods=["GET", "POST"])
 def museos2():
@@ -28,157 +33,138 @@ def inicio():
 @app.route('/ingresar', methods=["GET", "POST"])
 def cambioAIngresar():
     return render_template('ingresar.html')
-
-  
+ 
 @app.route('/verificar', methods=["GET", "POST"])
 def ingresar():
 
   conn = sqlite3.connect("artemis.db")
-  
   usuario=request.form["value"]
   contra=request.form["value2"]
   resu=conn.execute(f"""SELECT * FROM Administrador WHERE usuario='{usuario}' and contraseña='{contra}'""")
-  
+ 
   if resu.fetchone(): #Tengo usuario
       return(jsonify(True))
     #return redirect('/administrador')
   else:
       return(jsonify(False))
-    #return redirect('/inicio')
-  #else:
-    #return render_template('ingresar.html')
-    
-    
+       
 @app.route('/agregarIndex', methods=["POST"])
 def agregarIndex():
   if request.method == "POST":
     conn = sqlite3.connect("artemis.db")
 
-    search_term = request.form["value"]
-    search_term2 = request.files["value2"]
-
-        
-    q = f"""INSERT INTO Index(informacion, fotos) VALUES ('{search_term}', '{search_term2})"""
-    conn.execute(q)
+    search_term = request.form["informacion"]
+    file = request.files["fotos"]
+   
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+    print(file_path)
+   
+    
+    q = f"""SELECT * FROM Index"""
+    q = """
+        CREATE TABLE IF NOT EXISTS CualquierCosa (
+    informacion varchar(255),
+    fotos varchar(255)
+    );
+    """
+    q = f"""INSERT INTO CualquierCosa(informacion, fotos) VALUES ('{search_term}', '{filename}')"""
+    print(q)
+    res = conn.execute(q)
     conn.commit()
     conn.close()
-
-  return(jsonify(True))
+   
+    return(redirect('/administrador'))
+  else:
+    return(redirect('/administrador'))
 
 @app.route('/borrarIndex', methods=["DELETE"])
-def borrarIndex():
-  conn = sqlite3.connect('artemis.db')
-  q = conn.execute(f"""SELECT * FROM Index""")
-  mostrar = q.fetchall()
-  print(mostrar)
+def borrarIndexImagen():
+  SelectEliminarIndexImagen = request.form["value"]
+  conn = sqlite3.connect("artemis.db")
+  resu = (f"""DELETE FROM CualquierCosa WHERE fotos= '{SelectEliminarIndexImagen}'""")  
+  print(resu)
+  conn.execute(resu)
+  conn.commit()
   conn.close()
-  lenght = len(mostrar)
-
-  return render_template('', lenght=lenght, conn=mostrar)
-
+  return SelectEliminarIndexImagen
+ 
+  
 @app.route('/agregarMuseo', methods=["POST"])
 def agregarMuseo():
   if request.method == "POST":
     conn = sqlite3.connect("artemis.db")
+    search_term = request.form["titulo"]
+    search_term2 = request.form["informacion"]
+    search_term3 = request.form["coleccion"]
+    search_term4 = request.form["horario"]
+    search_term5 = request.form["precios"]
+    search_term6 = request.form["ubicacion"]
+    search_term7 = request.form["barrio"]
+    search_term8 = request.form["linkPagina"]
+    file = request.files["logo"]
+
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+    print(file_path)
     
-    search_term = request.form["value"]
-    search_term2 = request.form["value2"]
-    search_term3 = request.form["value3"]
-    search_term4 = request.form["value4"]
-    search_term5 = request.form["value5"]
-    search_term6 = request.form["value6"]
-    search_term7 = request.form["value7"]
-    search_term8 = request.form["value8"]
-    search_term9 = request.files["value9"]
+    q = f"""SELECT * FROM Museo"""
     
-    q = f"""INSERT INTO Museo(nombreMuseo, informacio, coleccionActual, horario, precio, ubicacion, barrio, linkPagina, logo) VALUES ('{search_term}', '{search_term2}', '{search_term3}', '{search_term4}', '{search_term5}', '{search_term6}', '{search_term7}', '{search_term8}', '{search_term9}')"""
+    q = f"""INSERT INTO Museo(nombreMuseo, informacio, coleccionActual, horario, precio, ubicacion, barrio, linkPagina, logo) VALUES ('{search_term}', '{search_term2}', '{search_term3}', '{search_term4}', '{search_term5}', '{search_term6}', '{search_term7}', '{search_term8}', '{filename}')"""
     conn.execute(q)
     conn.commit()
     conn.close()
+    print(q)
+    res = conn.execute(q)
+    conn.commit()
+    conn.close()
+   
+    return(redirect('/administrador'))
+  else:
+    return(redirect('/administrador'))
 
-  return(jsonify(True))
-
+ 
+@app.route('/borrarMuseos', methods=['DELETE'])
+def borrarMuseoos():  
+  SelectEliminarM = request.form["value"]
+  conn = sqlite3.connect("artemis.db")
+  resu = (f"""DELETE FROM Museo WHERE nombreMuseo= '{SelectEliminarM}'""")  
+  conn.execute(resu)
+  conn.commit()
+  conn.close()
+  return SelectEliminarM
 
 @app.route('/agregarPreguntas', methods=["GET","POST"])
 def agregarPreguntas():
   if request.method == "POST":
     conn = sqlite3.connect("artemis.db")
-      
     search_term = request.form["value"]
     search_term2 = request.form["value2"]
-    search_term3 = request.form["value3"]
-    search_term4 = request.form["value4"]
-    search_term5 = request.form["value5"]
-    search_term6 = request.form["value6"]
-    search_term7 = request.form["value7"]
-    search_term8 = request.form["value8"]
-    search_term9 = request.form["value9"]
-    search_term10 = request.form["value10"]
-    q = f"""INSERT INTO FAQ(pregunta1, respuesta1, pregunta2, respuesta2, pregunta3, respuesta3, pregunta4, respuesta4, pregunta5, respuesta5) VALUES ('{search_term}', '{search_term2}','{search_term3}', '{search_term4}', '{search_term5}','{search_term6}',' {search_term7}','{search_term8}','{search_term9}', '{search_term10}')"""
+   
+    q = f"""INSERT INTO FAQ(pregunta, respuesta ) VALUES ('{search_term}', '{search_term2}')"""
     conn.execute(q)
     conn.commit()
     conn.close()
 
-  
   return(jsonify(True))
-
 
 @app.route(('/borrarPreguntas'), methods=["DELETE"])
 def borrarPreguntas():
   SelectEliminar = request.form["value"]
   conn = sqlite3.connect("artemis.db")
-      
-  conne = sqlite3.connect('artemis.db')
-  qq = conne.execute(f"""SELECT * FROM FAQ""")
-  mostrar = qq.fetchall()
-  conne.close()
-  
-  if SelectEliminar in mostrar[-1][0]:
-    resu = (f"""INSERT INTO FAQ pregunta1 ='{SelectEliminar}'""")
-  elif SelectEliminar in mostrar[-1][2]:
-    resu = (f"""DELETE FROM FAQ WHERE pregunta2 ='{SelectEliminar}'""")
-  elif SelectEliminar in mostrar[-1][4]:
-    resu = (f"""DELETE FROM FAQ WHERE pregunta3 ='{SelectEliminar}'""")
-  elif SelectEliminar in mostrar[-1][6]:
-    resu = ( f"""DELETE FROM FAQ WHERE pregunta4 ='{SelectEliminar}'""")
-  else :
-   resu = (f"""DELETE FROM FAQ WHERE pregunta5 ='{SelectEliminar}'""")
-    
+  resu = (f"""DELETE FROM FAQ WHERE id= '{SelectEliminar}'""")  
+  conn.execute(resu)
   conn.commit()
   conn.close()
-
   return SelectEliminar
-  
-''' SelectEliminar = request.form["value"]
-    conexion = sqlite3.connect('artemis.db')
-    cursor = conexion.cursor()
-    resu=cursor.execute(f"""SELECT * FROM FAQ""")
-
-    usuario = cursor.fetchone()
-  
-    if SelectEliminar== '{pregunta1}':
-      resu = (f"""DELETE FROM FAQ WHERE pregunta1 ='{SelectEliminar}'""")
-    elif SelectEliminar== '{pregunta2}':
-      resu = (f"""DELETE FROM FAQ WHERE pregunta2 ='{SelectEliminar}'""")
-    elif SelectEliminar== '{pregunta3}':
-      resu = (f"""DELETE FROM FAQ WHERE pregunta3 ='{SelectEliminar}'""")
-    elif SelectEliminar== '{pregunta4}':
-      resu = ( f"""DELETE FROM FAQ WHERE pregunta4 ='{SelectEliminar}'""")
-    else :
-     resu = (f"""DELETE FROM FAQ WHERE pregunta5 ='{SelectEliminar}'""")
-     
-    print(resu)
-    cursor.execute(resu)
-    cursor.commit()
-    cursor.close()
-    
-'''
-  
+ 
 @app.route('/agregarNovedades', methods=['GET', 'POST'])
 def agregarNovedades():
   if request.method=="POST":
     conn=sqlite3.connect('artemis.db')
-    
+   
     search_term = request.files["value"]
     search_term2 = request.form["value2"]
     search_term3 = request.files["value3"]
@@ -193,35 +179,66 @@ def agregarNovedades():
     conn.commit()
     conn.close()
 
-  
   return(jsonify(True))
-    
-
-@app.route('/agregarInternacional', methods=["GET", "POST"])
+   
+@app.route('/agregarInternacional', methods=["POST"])
 def agregarInternacional():
   if request.method == "POST":
     conn = sqlite3.connect("artemis.db")
 
-    search_term = request.form["value"]
-    search_term2 = request.form["value2"]
-    search_term3 = request.files["value3"]
-    search_term4 = request.form["value4"]
-    search_term5 = request.form["value5"]
-    search_term6 = request.files["value6"]
-    search_term7 = request.form["value7"]
-    search_term8 = request.form["value8"]
-    search_term9 = request.files["value9"]
-    q = f"""INSERT INTO Internacionales(nombreCampo1, textoCampo1, fotoCampo1, nombreCampo2, textoCampo2, fotoCampo2, nombreCampo3, textoCampo3, fotoCampo3) VALUES ('{search_term}', '{search_term2}','{search_term3}', '{search_term4}', '{search_term5}','{search_term6}',' {search_term7}','{search_term8}','{search_term9}')"""
-    conn.execute(q)
+    search_term = request.form["campo1"]
+    search_term2 = request.form["textocampo1"]
+    file = request.files["fotoCampo1"]
+    search_term4 = request.form["campo2"]
+    search_term5 = request.form["textocampo2"]
+    file2 = request.files["fotoCampo2"]
+    search_term7 = request.form["campo3"]
+    search_term8 = request.form["textocampo3"]
+    file3 = request.files["fotoCampo3"]
+    
+    filename = secure_filename(file.filename)
+    filename2 = secure_filename(file2.filename)
+    filename3 = secure_filename(file3.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename, filename2, filename3)
+    file.save(file_path)
+    print(file_path)
+    q = f"""SELECT * FROM Internacionales"""
+    q = """
+        CREATE TABLE IF NOT EXISTS internacional2 (
+    nombreCampo1 varchar(255),
+    textoCampo1 varchar(255),
+    fotoCampo1 varchar(255),
+    nombreCampo2 varchar(255),
+    textoCampo2 varchar(255),
+    fotoCampo2 varchar(255),
+    nombreCampo3 varchar(255),
+    textoCampo3 varchar(255),
+    fotoCampo3 varchar(255),
+    
+    );
+    """
+    q = f"""INSERT INTO internacional2((nombreCampo1, textoCampo1, fotoCampo1, nombreCampo2, textoCampo2,fotoCampo2, nombreCampo3, textoCampo3, fotoCampo3) VALUES ('{search_term}','{search_term2}' ,'{filename}', '{search_term4}', '{search_term5}','{filename2}',' {search_term7}','{search_term8}','{filename3}')"""
+    print(q)
+    res = conn.execute(q)
     conn.commit()
     conn.close()
-
   
-  return(jsonify(True))
+    return(redirect('/administrador'))
+  else:
+    return(redirect('/administrador'))
 
 @app.route('/museos', methods=["GET", "POST"])
 def museos():
-    return render_template('museos.html')
+  conn = sqlite3.connect("artemis.db")
+  resu=conn.execute(f"""SELECT * FROM Museo """)
+  respuestaMuseos= resu.fetchall()
+     #pedido a la base
+
+    #me traigo datos
+  conn.close()  
+  return render_template('museos.html',
+                        respuestaMuseos=respuestaMuseos,             lenght=len(respuestaMuseos))
+   
 
 @app.route('/novedades', methods=["GET", "POST"])
 def novedades():
@@ -231,11 +248,17 @@ def novedades():
 def administrador():  
   conn = sqlite3.connect('artemis.db')
   q = conn.execute(f"""SELECT * FROM FAQ""")
+  r = conn.execute(f"""SELECT * FROM Museo""")
+  i= conn.execute(f"""SELECT * FROM CualquierCosa""")
+  mostrarM= r.fetchall()
   mostrar = q.fetchall()
+  mostrarI= i.fetchall()
   conn.close()
-  lenght = len(mostrar[-1])
-  return render_template('administrador.html', lenght = lenght, mostrar = mostrar)
-  
+  lenghtI=len(mostrarI)
+  lenghtM=len(mostrarM)
+  lenght = len(mostrar)
+  return render_template('administrador.html', lenght = lenght, mostrar = mostrar, lenghtM=lenghtM, lenghtI=lenghtI, mostrarI=mostrarI)
+ 
 @app.route('/internacional', methods=["GET", "POST"])
 def internacional():
   return render_template('internacional.html')
@@ -247,11 +270,10 @@ def faq():
   respuestaDeLaBaseDeDatos= resu.fetchall()
      #pedido a la base
 
-      #me traigo datos
-  
+    #me traigo datos
   conn.close()  
-  return render_template('faq.html', 
-                        respuestaDeLaBaseDeDatos = respuestaDeLaBaseDeDatos)
-  
+  return render_template('faq.html',
+                        respuestaDeLaBaseDeDatos = respuestaDeLaBaseDeDatos, lenght = len(respuestaDeLaBaseDeDatos))
+ 
 
 app.run(host='0.0.0.0', port=81)
